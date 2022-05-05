afdeede3917f13f5ca608a6964319a4be7affc79
narenallam <naren.allam@gmail.com>
2022-05-05 10:44:35 +0530

    change made to insurance

--- insurance.py
+++ insurance.py
@@ -3,13 +3,13 @@
 # fermale and above 25 years age
 
 married = input("Enter marrital status[m/u]:")
-gender = input("Enter gender [m/f]:")
-age = int(input("Enter age:"))
 
 if married == 'm':
     print("Insured")
-
+    
 else:
+    gender = input("Enter gender [m/f]:")
+    age = int(input("Enter age:"))
     if (gender == 'm' and age > 30) or (gender == 'f' and age > 25):
         print("Insured")
     else:
