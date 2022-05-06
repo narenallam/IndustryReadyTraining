577046672e149b2f58569f64d66571c42fd2683a
narenallam <naren.allam@gmail.com>
2022-05-06 11:26:29 +0530

    triangles added

--- for_loop.py
+++ for_loop.py
@@ -1,7 +1,12 @@
-'''
-This is a basic program
-to use for loop
-'''
-
-for x in range(10):
-    print("Apple")
+"""
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+"""
+    
+for i in range(5, 0, -1):
+    for j in range(1, i+1):
+        print(i, end=' ')
+    print()
